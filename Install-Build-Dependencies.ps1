fe4530dc8004573a205826b3002ad43cc0c13d40
techy804 <elijahworden@gmail.com>
2025-08-09 22:19:43 -0400

    Updated version of GM3P that would be installed through the script

--- Install-Build-Dependencies.ps1
+++ Install-Build-Dependencies.ps1
@@ -7,8 +7,8 @@ winget install CosimoMatteini.DRA
 winget install Meld
 # then install GM3P
 New-Item -Path ".\" -Name "gm3p" -ItemType Directory
-dra download --output ".\" --tag "v0.5.0" --select "GM3P.v0.5.0.zip" techy804/MassModPatcher
-Expand-Archive ".\GM3P.v0.5.0.zip" -DestinationPath ".\gm3p" -Force
-del ".\GM3P.v0.5.0.zip"
+dra download --output ".\" --tag "v0.5.1" --select "GM3P.v0.5.1.zip" techy804/MassModPatcher
+Expand-Archive ".\GM3P.v0.5.1.zip" -DestinationPath ".\gm3p" -Force
+del ".\GM3P.v0.5.1.zip"
 # Lastly, install DeltaMOD's node modules
 npm i
