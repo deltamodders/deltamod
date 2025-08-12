d398d2d8c6d4b7b9690324fd3c8bf115db9cf3fe
techy804 <elijahworden@gmail.com>
2025-08-11 21:48:35 -0400

    Replaced Meld with Git Added a string var to easily change target GM3P version for future updates

--- Install-Build-Dependencies.ps1
+++ Install-Build-Dependencies.ps1
@@ -4,11 +4,12 @@ winget install OpenJS.NodeJS.LTS
 # then install GM3P dependiencies
 winget install Microsoft.DotNet.SDK.8
 winget install CosimoMatteini.DRA
-winget install Meld
+winget install Microsoft.Git
 # then install GM3P
+$GM3Pver = "v0.5.1"
 New-Item -Path ".\" -Name "gm3p" -ItemType Directory
-dra download --output ".\" --tag "v0.5.1" --select "GM3P.v0.5.1.zip" techy804/MassModPatcher
-Expand-Archive ".\GM3P.v0.5.1.zip" -DestinationPath ".\gm3p" -Force
-del ".\GM3P.v0.5.1.zip"
+dra download --output ".\" --tag $GM3Pver --select "GM3P.$GM3Pver.zip" techy804/MassModPatcher
+Expand-Archive ".\GM3P.$GM3Pver.zip" -DestinationPath ".\gm3p" -Force
+del ".\GM3P.$GM3Pver.zip"
 # Lastly, install DeltaMOD's node modules
 npm i
