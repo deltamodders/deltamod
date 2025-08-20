4e5b452a1b93dbaea548f91ff4d27ad821f6ff14
techy804 <elijahworden@gmail.com>
2025-08-20 06:25:31 -0400

    Added prompt to run script

--- Run-Linux.sh
+++ Run-Linux.sh
@@ -1,55 +1,99 @@
 #!/bin/bash
 # If editing on Windows, remember to run this command on WSL before committing and/or testing: "sed -i 's/\r//g' Run-Linux.sh"
-sudo snap install 'dotnet-sdk' --classic
 
-#Install node through docker
+#prompt user on if they want to install dependencies
 
-sudo apt install 'docker'
+goto() {
+  label=$1
+  cmd=$(sed -En "/^[[:space:]]*#[[:space:]]*$label:[[:space:]]*#/{:a;n;p;ba};" "$0")
+  eval "$cmd"
+  exit
+}
 
-# Docker has specific installation instructions for each operating system.
-# Please refer to the official documentation at https://docker.com/get-started/
+prompt_for_input() {
+    while true; do
+        echo -n "Do you want to install or update dependencies? (y/n): "
+        read -r response
+        case "$response" in
+            [Yy]* )
+                echo "You selected Yes."
+				goto install
+                return 0
+                ;;
+            [Nn]* )
+                echo "You selected No."
+				goto run
+                return 1
+                ;;
+            [Cc]* )
+                echo "You selected Cancel."
+                return 2
+                ;;
+            * )
+                echo "Invalid input. Please enter y or n."
+                ;;
+        esac
+    done
+}
 
-# Pull the Node.js Docker image:
-docker pull node:22-alpine
+prompt_for_input	
 
-# Create a Node.js container and start a Shell session:
-docker run -it --rm --entrypoint sh node:22-alpine
+#install:#
+		#Intall .NET SDK 8.0
 
-# Verify the Node.js version:
-node -v # Should print "v22.18.0".
+		sudo snap install 'dotnet-sdk' --classic
 
-# Verify npm version:
-npm -v # Should print "10.9.3".
+		#Install node through docker
+
+		sudo apt install 'docker'
+
+		# Docker has specific installation instructions for each operating system.
+		# Please refer to the official documentation at https://docker.com/get-started/
+
+		# Pull the Node.js Docker image:
+		docker pull node:22-alpine
+
+		# Create a Node.js container and start a Shell session:
+		docker run -it --rm --entrypoint sh node:22-alpine
+
+		# Verify the Node.js version:
+		node -v # Should print "v22.18.0".
+
+		# Verify npm version:
+		npm -v # Should print "10.9.3".
 
 
-#Install needed libraries
+		#Install needed libraries
 
-sudo apt install 'libnspr4'
+		sudo apt install 'libnspr4'
 
-sudo apt install 'libnss3'
+		sudo apt install 'libnss3'
 
-#Install things needed to download and install GM3P
+		#Install things needed to download and install GM3P
 
-sudo apt install 'wget'
+		sudo apt install 'wget'
 
-sudo apt-get install 'unzip'
+		sudo apt-get install 'unzip'
 
-sudo apt-get install 'xdelta3'
+		sudo apt-get install 'xdelta3'
 
-#Install GM3P
+		#Install GM3P
 
-wget https://github.com/techy804/MassModPatcher/releases/download/v0.6.0-alpha1/GM3P.v0.6.0-alpha1.zip
+		wget https://github.com/techy804/MassModPatcher/releases/download/v0.6.0-alpha1/GM3P.v0.6.0-alpha1.zip
 
-unzip -f -o "./GM3P.v0.6.0-alpha1.zip" -d "./gm3p"
+		unzip -f -o "./GM3P.v0.6.0-alpha1.zip" -d "./gm3p"
 
-rm './GM3P.v0.6.0-alpha1.zip'
+		rm './GM3P.v0.6.0-alpha1.zip'
 
-#Install needed npm packages and run
+		#Install needed npm packages and run
 
-npm --verbose install electron@37
+		npm --verbose install electron@37
 
-npm install n 22
+		npm install n@22
 
-npm i
+		goto run
 
-npm test
+#run:#
+		npm i
+
+		npm test
