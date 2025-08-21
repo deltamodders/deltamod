1b4e585e7efc1e071dbc7c7f7dca8cb83f57431c
ghinorhino <218945672+ghinorhino@users.noreply.github.com>
2025-08-21 11:19:22 +0200

    Modify UUID generator to use Utils

--- node/System.js
+++ node/System.js
@@ -1,17 +1,8 @@
 const fs = require('fs');
 const app = require('electron').app;
 const path = require('path');
-
+const { randomString } = require('./Utils');
 let systemIndex = "0";
-
-function randomString(length) {
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+[]{}|;:,.<>?';
-    let result = '';
-    for (let i = 0; i < length; i++) {
-        result += characters.charAt(Math.floor(Math.random() * characters.length));
-    }
-    return result;
-}
 
 function generateUniqueId() {
     return "deltamod_" + randomString(16) + "_" + Date.now() + "_" + require('../package.json').version;
