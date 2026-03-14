4e5b8190aa8543049fef70e5147fb08041f7d908
ghinorhino <218945672+ghinorhino@users.noreply.github.com>
2026-03-14 09:02:48 +0100

    corrections

--- node/Sentry.js
+++ node/Sentry.js
@@ -2,7 +2,7 @@ const Sentry = require("@sentry/node");
 const console = require('./Console.js');
 Sentry.init({
   dsn: "https://9a431a7b0594d5745a7148c4bba1216a@o4511026290098176.ingest.de.sentry.io/4511026291736656",
-  sendDefaultPii: true,
+  sendDefaultPii: false,
 });
 
 console.log('Sentry initialized');
