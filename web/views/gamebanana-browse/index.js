1578d4d549fd47bb9a48d1b7f7879a88625fb579
cmdr-chara <249489759+cmdr-chara@users.noreply.github.com>
2026-07-27 13:54:45 +0200

    Remove hard-coded GameBanana submission exclusion

--- web/views/gamebanana-browse/index.js
+++ web/views/gamebanana-browse/index.js
@@ -1,9 +1,5 @@
 let PAGE = (window._pageArguments && window._pageArguments.lp) ? parseInt(window._pageArguments.lp) : 1;
 
-const BLACKLIST_MODS = [
-    493817 // Gendered Kris mod
-];
-
 window.PAGE = PAGE;
 
 window._onClosePage.push(() => {
@@ -212,7 +208,6 @@ async function renderMods(table, GB_API, filter, gameID) {
         for (const mod of data._aRecords) {
             if (mod._sModelName == 'Wip' && !mod._bHasFiles) continue;
             if (mod._sModelName != 'Wip' && mod._sModelName != 'Mod') continue;
-            if (BLACKLIST_MODS.includes(mod._idRow)) continue;
             
             await (async () => {
                 var tr = document.createElement('tr');
