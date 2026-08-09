4026408457c4f05350e60df6194f786daa607ee9
techy804 <elijahworden@gmail.com>
2026-08-09 18:41:50 -0400

    Possible Folder Bug Fix https://gamebanana.com/posts/13812838

--- node/GamePatching.js
+++ node/GamePatching.js
@@ -156,6 +156,9 @@ async function startGamePatch(gamePath, modFolder, mods, logCallback, progressCa
                 fs.renameSync(targetPath, targetPath + '.bak');
             }
             else {
+                if (!fs.existsSync(path.dirname(targetPath).)) {
+                    fs.mkdirSync(path.dirname(targetPath), { recursive: true });
+                }
                 fs.writeFileSync(targetPath + '.rem', "");
             }
             
